340648bf27655b18fd5fc5bf5244f9ad16a03c2c
lijian <18252082598@163.com>
2018-03-05 17:43:00 +0800

    添加定时器相关注释

--- ssm/src/main/java/com/joymeter/scheduled/MyScheduledTest.java
+++ ssm/src/main/java/com/joymeter/scheduled/MyScheduledTest.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyScheduledTest {
 
-    @Scheduled(cron="0/5 * * * * ? ")
+    @Scheduled(cron="0/5 * * * * ? ")//每隔5秒调用一次http://cron.qqe2.com/ 在线cron表达式生成
     public void Mytest() {
         System.out.println("进入测试：");
     }
