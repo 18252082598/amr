0c2e089a13bf1ef17fd2fd7258523a28832b4ae9
lijan <18252082598@163.com>
2018-03-20 15:07:19 +0800

    添加IO相关的具体操作demo

---
+++ ssm/src/main/java/com/joymeter/information/OutPutStream.java
@@ -0,0 +1,38 @@
+package com.joymeter.information;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+/**
+ * 字节流的写入
+ * @author joymeter
+ *
+ */
+public class OutPutStream {
+    public static void main(String[] args ) {  
+        
+        String path = "D:\\IO\\demo.txt";   
+           
+        FileOutputStream o = null;  
+           
+        try {  
+            o = new FileOutputStream(path);  
+            String str = "Nerxious is a good boy\r\n";  
+            byte[] buf = str.getBytes();  
+            //也可以直接使用o.write("String".getBytes());  
+            //因为字符串就是一个对象，能直接调用方法  
+            o.write(buf);  
+               
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            if(o != null) {  
+                try {  
+                    o.close();  
+                } catch (IOException e) {  
+                    e.printStackTrace();  
+                }  
+            }  
+        }  
+      
+    }  
+}
